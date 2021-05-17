1e2e26b328998c0a80118356a393cff27a305fc1
TheDoctor <TheDoctor@Gallifrey.org>
2021-05-17 13:36:42 -0700

    Add project files.

---
+++ UWPXAML_CheckboxDescribedBy/MainPage.xaml.cs
@@ -0,0 +1,17 @@
+﻿using Windows.UI.Xaml.Automation;
+using Windows.UI.Xaml.Controls;
+
+namespace UWPXAML_CheckboxDescribedBy
+{
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+
+            // Have the CheckBox described by a nearby TextBlock.
+            var describedBy = AutomationProperties.GetDescribedBy(PreloadCheckbox);
+            describedBy.Add(PreloadDescription);
+        }
+    }
+}
